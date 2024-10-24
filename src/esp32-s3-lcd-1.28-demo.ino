daa23ad2904ad151b893261042bd2ff8f2b3ce39
Ruben Schlonsak <rs28097@gmail.com>
2024-10-24 10:24:38 +0200

    working demo display , BLE information

--- src/esp32-s3-lcd-1.28-demo.ino
+++ src/esp32-s3-lcd-1.28-demo.ino
@@ -39,9 +39,9 @@ void setup()
     Serial.begin(115200);
 
     // BLE Initialization
-    BLEDevice::init("ESP32-BLE-HelloWorld");
+    BLEDevice::init("ESP32-BLE-AccelGyro");
     pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks()); // Set the callback for connection state
+    pServer->setCallbacks(new MyServerCallbacks());
 
     BLEService *pService = pServer->createService(SERVICE_UUID);
     pCharacteristic = pService->createCharacteristic(
@@ -51,7 +51,7 @@ void setup()
                         BLECharacteristic::PROPERTY_NOTIFY |
                         BLECharacteristic::PROPERTY_INDICATE
                       );
-    pCharacteristic->setValue("Hello World");
+    pCharacteristic->setValue("AccelGyro Data");
     pService->start();
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->start();
@@ -116,16 +116,65 @@ void setup()
     DEV_Delay_ms(1000);
 #endif
 
+#if 1
+    float acc[3], gyro[3];
+    unsigned int tim_count = 0;
+    uint16_t result;
+
+    QMI8658_init();
+    Serial.println("QMI8658_init\r\n");
+    // DEV_SET_PWM(100);
+    const float conversion_factor = 3.3f / (1 << 12) * 3;
+    Paint_Clear(WHITE);
+    Paint_DrawRectangle(0, 00, 240, 47, 0XF410, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+    Paint_DrawRectangle(0, 47, 240, 120, 0X4F30, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+    Paint_DrawRectangle(0, 120, 240, 195, 0XAD55, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+    Paint_DrawRectangle(0, 195, 240, 240, 0X2595, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+
+    Paint_DrawString_EN(45, 30, "LongPress Quit", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 50, "ACC_X = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 75, "ACC_Y = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 100, "ACC_Z = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 125, "GYR_X = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 150, "GYR_Y = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 175, "GYR_Z = ", &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(45, 200, "BAT(V)=", &Font16, WHITE, BLACK);
+    LCD_1IN28_Display(BlackImage);
+
+    while (true)
+    {
+        result = DEC_ADC_Read();
+        QMI8658_read_xyz(acc, gyro, &tim_count);
+        Paint_Clear(WHITE);
+        Paint_DrawRectangle(120, 47,  220, 120, 0X4F30, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+        Paint_DrawRectangle(120, 120, 220, 195, 0XAD55, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+        Paint_DrawRectangle(120, 195, 220, 240, 0X2595, DOT_PIXEL_2X2, DRAW_FILL_FULL);
+        Paint_DrawNum(120, 50, acc[0], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(120, 75, acc[1], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(120, 100, acc[2], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(120, 125, gyro[0], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(120, 150, gyro[1], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(120, 175, gyro[2], &Font16, 2, BLACK, WHITE);
+        Paint_DrawNum(130, 200, result * conversion_factor, &Font16, 2, BLACK, WHITE);
+        LCD_1IN28_DisplayWindows(120, 50, 210, 200, BlackImage);
+        LCD_1IN28_DisplayWindows(130, 200, 220, 220, BlackImage);
+
+        // Send data over BLE
+        if (deviceConnected) {
+            char dataStr[100];
+            snprintf(dataStr, sizeof(dataStr), "ACC: %.2f, %.2f, %.2f; GYRO: %.2f, %.2f, %.2f",
+                     acc[0], acc[1], acc[2], gyro[0], gyro[1], gyro[2]);
+            pCharacteristic->setValue(dataStr);
+            pCharacteristic->notify();
+        }
+
+        delay(100); // 0.1 second delay
+    }
+#endif
     delay(2000);
 }
 
 void loop()
 {
-    // Send "Hello World" over BLE every 5 seconds if device is connected
-    if (deviceConnected) {
-        pCharacteristic->setValue("Hello World");
-        pCharacteristic->notify();
-        delay(5000);  // 5 seconds delay
-    }
-    delay(5000);
+    // Empty loop since the main code runs in setup()
 }
